4f7c5937d9923e7238e4b7a7ffb9e815e589e603
toxa055 <toxa055@gmail.com>
2021-10-29 13:24:06 +0300

    Add cure creating validation.

--- src/main/java/com/rehab/controller/CureController.java
+++ src/main/java/com/rehab/controller/CureController.java
@@ -8,13 +8,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/cures")
 @Secured({"ROLE_ADMIN", "ROLE_DOCTOR", "ROLE_NURSE"})
 public class CureController {
 
+    private static final String NEW_CURE_URL = "/cures/new";
+    private static final String CURE_URL = "/cures/cure";
+    private static final String CURE = "cure";
     private final CureService cureService;
 
     @Autowired
@@ -22,25 +30,34 @@ public class CureController {
         this.cureService = cureService;
     }
 
-    @GetMapping(value = "/getBy", params = "id")
-    public String getById(@RequestParam int id, Model model) {
-        model.addAttribute("cure", cureService.getById(id));
-        return "/cures/cure";
+    @GetMapping("/{id}")
+    public String getById(@PathVariable int id, Model model) {
+        model.addAttribute(CURE, cureService.getById(id));
+        return CURE_URL;
     }
 
-    @GetMapping(value = "getBy", params = "name")
-    public String getByName(@RequestParam String name, Model model) {
-        model.addAttribute("cure", cureService.getByName(name));
-        return "cures/cure";
+    @GetMapping("/name/{name}")
+    public String getByName(@PathVariable String name, Model model) {
+        model.addAttribute(CURE, cureService.getByName(name));
+        return CURE_URL;
     }
 
     @GetMapping("/new")
     public String create() {
-        return "/cures/new";
+        return NEW_CURE_URL;
     }
 
     @PostMapping("new")
-    public String createCure(CureDto cureDto) {
+    public String createCure(@Valid CureDto cureDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            var errorsMap = bindingResult.getFieldErrors().stream()
+                    .collect(Collectors.toMap(f -> f.getField() + "Error", FieldError::getDefaultMessage,
+                            (m1, m2) -> String.join("<br>", m1, m2)));
+            model.addAllAttributes(errorsMap);
+            model.addAttribute("name", cureDto.getName());
+            model.addAttribute("cureType", cureDto.getCureType());
+            return NEW_CURE_URL;
+        }
         cureService.save(cureDto);
         return "redirect:";
     }
--- src/main/java/com/rehab/dto/CureDto.java
+++ src/main/java/com/rehab/dto/CureDto.java
@@ -2,13 +2,15 @@ package com.rehab.dto;
 
 import com.rehab.model.type.CureType;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class CureDto {
     private int id;
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 3, max = 30, message = "Length must be from 3 to 30 symbols")
     private String name;
     private CureType cureType;
-
-    public CureDto() {
-    }
 
     public int getId() {
         return id;
