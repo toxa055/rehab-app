99a81a92ddc67b2c2b233a7bad61582b9766d81d
toxa055 <toxa055@gmail.com>
2021-11-23 21:50:21 +0300

    Logging for Rest controllers.

--- src/main/java/com/rehab/controller/RestPatientController.java
+++ src/main/java/com/rehab/controller/RestPatientController.java
@@ -2,6 +2,7 @@ package com.rehab.controller;
 
 import com.rehab.dto.PatientDto;
 import com.rehab.service.PatientService;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.*;
 public class RestPatientController {
 
     private final PatientService patientService;
+    private final Logger logger;
 
     @Autowired
-    public RestPatientController(PatientService patientService) {
+    public RestPatientController(PatientService patientService, Logger logger) {
         this.patientService = patientService;
+        this.logger = logger;
     }
 
     @GetMapping("/rest/patients/{insNum}")
     public PatientDto getByInsNum(@PathVariable int insNum) {
+        logger.info("Get patient by insurance number {} through rest.", insNum);
         return patientService.getByInsuranceNumber(insNum);
     }
 }
