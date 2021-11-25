9a215a51204329e96a4206d67e4b15a31efaea20
toxa055 <toxa055@gmail.com>
2021-11-25 19:36:47 +0300

    Javadoc for Cure/Period/Pattern- CrudRepository.

--- src/main/java/com/rehab/repository/CureCrudRepository.java
+++ src/main/java/com/rehab/repository/CureCrudRepository.java
@@ -9,11 +9,29 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Interface which allows working with cures as entities in database.
+ *
+ * @see Cure
+ */
 @Repository
 public interface CureCrudRepository extends JpaRepository<Cure, Integer> {
 
+    /**
+     * Method to get only cure by its name ignoring case.
+     *
+     * @param name particular cure name, must not be null.
+     * @return optional value of found cure.
+     */
     Optional<Cure> findByNameIgnoreCase(String name);
 
+    /**
+     * Method to get cures whose names contain value of nameLike ignoring case.
+     *
+     * @param nameLike not a particular cure name, but char sequence which cure names have to contain.
+     * @param pageable interface that provides pagination.
+     * @return page of all the cures if nameLike is null or only found cures.
+     */
     @Query("SELECT c FROM Cure c WHERE :nameLike IS NULL OR LOWER(c.name) LIKE %:nameLike%")
     Page<Cure> filter(String nameLike, Pageable pageable);
 }
--- src/main/java/com/rehab/repository/PeriodCrudRepository.java
+++ src/main/java/com/rehab/repository/PeriodCrudRepository.java
@@ -7,8 +7,20 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Interface which allows working with periods as entities in database.
+ *
+ * @see Period
+ */
 @Repository
 public interface PeriodCrudRepository extends JpaRepository<Period, Integer> {
 
+    /**
+     * Method to get only period by its count and time unit.
+     *
+     * @param count particular count, must not be null.
+     * @param unit  particular time unit, must not be null.
+     * @return optional value of found period.
+     */
     Optional<Period> findByCountAndUnit(Integer count, TimeUnit unit);
 }
