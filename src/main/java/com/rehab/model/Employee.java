d747c7b6dff3f7d8afe0e19ec6635ceab6eab87f
toxa055 <toxa055@gmail.com>
2021-11-25 13:08:29 +0300

    Javadoc for Employee.

--- src/main/java/com/rehab/model/Employee.java
+++ src/main/java/com/rehab/model/Employee.java
@@ -5,22 +5,55 @@ import com.rehab.model.type.Role;
 import javax.persistence.*;
 import java.util.*;
 
+/**
+ * Entity describing employee of rehabilitation hospital which is a user of this application.
+ * Since it extends {@link AbstractIdEntity} it has id.
+ * It maps to table 'employees' in database.
+ */
 @Entity
 @Table(name = "employees")
 public class Employee extends AbstractIdEntity {
 
+    /**
+     * Employee's name, second name.
+     * It maps to column 'name' to table 'employees' in database.
+     * Value cannot be null.
+     */
     @Column(name = "name", nullable = false)
     private String name;
 
+    /**
+     * Employee's job position.
+     * It maps to column 'position' to table 'employees' in database.
+     * Value cannot be null.
+     */
     @Column(name = "position", nullable = false)
     private String position;
 
+    /**
+     * Employee's unique email address. It's used for logging to the application.
+     * It maps to column 'email' to table 'employees' in database.
+     * Value cannot be null.
+     */
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    /**
+     * Employee's password. It's used for logging to the application.
+     * It maps to column 'password' to table 'employees' in database.
+     * Value cannot be null.
+     */
     @Column(name = "password", nullable = false)
     private String password;
 
+    /**
+     * Employee's roles. They're used for defining what functionality is available for current employee.
+     * It maps to column 'role' to table 'employee_roles' in database.
+     * Table 'employee_roles' connects to table 'employees' with 'employee_id'.
+     * Value cannot be null.
+     *
+     * @see Role
+     */
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     @CollectionTable(name = "employee_roles", joinColumns = @JoinColumn(name = "employee_id"),
@@ -29,12 +62,22 @@ public class Employee extends AbstractIdEntity {
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "doctor")
-    private List<Prescription> prescriptions = new ArrayList<>();
-
+    /**
+     * Empty constructor. It's mandatory for Hibernate.
+     */
     public Employee() {
     }
 
+    /**
+     * Constructor to initialize following fields.
+     *
+     * @param id       description of id is in field declaration.
+     * @param name     description of name is in field declaration.
+     * @param position description of position is in field declaration.
+     * @param email    description of email is in field declaration.
+     * @param password description of password is in field declaration.
+     * @param roles    description of roles is in field declaration.
+     */
     public Employee(Integer id, String name, String position, String email, String password, Set<Role> roles) {
         super(id);
         this.name = name;
@@ -64,10 +107,6 @@ public class Employee extends AbstractIdEntity {
         return roles;
     }
 
-    public List<Prescription> getPrescriptions() {
-        return prescriptions;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -86,9 +125,5 @@ public class Employee extends AbstractIdEntity {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
-    }
-
-    public void setPrescriptions(List<Prescription> prescriptions) {
-        this.prescriptions = prescriptions;
     }
 }
