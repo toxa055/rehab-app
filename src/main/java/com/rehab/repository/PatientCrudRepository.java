f26eb4f3d689db68a7cd1c50f71ec859682ac680
toxa055 <toxa055@gmail.com>
2021-11-25 20:34:51 +0300

    Javadoc for Patient/Employee- CrudRepository.

--- src/main/java/com/rehab/repository/PatientCrudRepository.java
+++ src/main/java/com/rehab/repository/PatientCrudRepository.java
@@ -10,9 +10,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Interface which allows working with patients as entities in database.
+ *
+ * @see Patient
+ */
 @Repository
 public interface PatientCrudRepository extends JpaRepository<Patient, Integer> {
 
+    /**
+     * Method to get patients filtering by given parameters.
+     *
+     * @param insuranceNumber particular insurance number.
+     * @param nameLike        not a particular patient name, but char sequence which patient names have to contain.
+     * @param treating        patient state: only treating or not.
+     * @param pageable        interface that provides pagination.
+     * @return page of found patients by given parameters.
+     */
     @Query("""
             SELECT p FROM Patient p
             WHERE (:insuranceNumber IS NULL OR p.insuranceNumber=:insuranceNumber)
@@ -21,5 +35,11 @@ public interface PatientCrudRepository extends JpaRepository<Patient, Integer> {
             """)
     Page<Patient> filter(Integer insuranceNumber, String nameLike, PatientState treating, Pageable pageable);
 
+    /**
+     * Method to get only patient by given insurance number.
+     *
+     * @param insuranceNumber particular insurance number, must not be null.
+     * @return optional value of found patient.
+     */
     Optional<Patient> findByInsuranceNumber(int insuranceNumber);
 }
