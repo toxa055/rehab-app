6942350dee14453f99be811c2991cce08979736c
toxa055 <toxa055@gmail.com>
2021-11-25 18:46:38 +0300

    Javadoc for Enums.

--- src/main/java/com/rehab/model/type/EventState.java
+++ src/main/java/com/rehab/model/type/EventState.java
@@ -1,7 +1,22 @@
 package com.rehab.model.type;
 
+/**
+ * Enumeration which stores event states.
+ */
 public enum EventState {
+
+    /**
+     * State for event that has to be performed by nurse at a particular date and time.
+     */
     PLANNED,
+
+    /**
+     * State for event that was performed by nurse.
+     */
     PERFORMED,
+
+    /**
+     * State for event that cancelled by nurse or doctor.
+     */
     CANCELLED
 }
--- src/main/java/com/rehab/model/type/PatternUnit.java
+++ src/main/java/com/rehab/model/type/PatternUnit.java
@@ -1,15 +1,66 @@
 package com.rehab.model.type;
 
+/**
+ * Enumeration which stores pattern units.
+ * They are used in {@link com.rehab.model.Pattern} to define what time of day or day of week
+ * patient has to take medicine (or to be treated with procedure).
+ *
+ * @see com.rehab.util.EventUtil
+ */
 public enum PatternUnit {
+
+    /**
+     * Unit which defines time of day as morning.
+     */
     MORNING,
+
+    /**
+     * Unit which defines time of day as afternoon.
+     */
     AFTERNOON,
+
+    /**
+     * Unit which defines time of day as evening.
+     */
     EVENING,
+
+    /**
+     * Unit which defines time of day as night.
+     */
     NIGHT,
+
+    /**
+     * Unit which defines day of week as Monday.
+     */
     MONDAY,
+
+    /**
+     * Unit which defines day of week as Tuesday.
+     */
     TUESDAY,
+
+    /**
+     * Unit which defines day of week as Wednesday.
+     */
     WEDNESDAY,
+
+    /**
+     * Unit which defines day of week as Thursday.
+     */
     THURSDAY,
+
+    /**
+     * Unit which defines day of week as Friday.
+     */
     FRIDAY,
+
+    /**
+     * Unit which defines day of week as Saturday.
+     */
     SATURDAY,
+
+    /**
+     * Unit which defines day of week as Sunday.
+     */
     SUNDAY
 }
