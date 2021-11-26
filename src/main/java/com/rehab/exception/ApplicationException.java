5eee53db61033b9f21e98ea533f0bf42b71db091
toxa055 <toxa055@gmail.com>
2021-11-26 11:41:17 +0300

    Javadoc for Exceptions.

--- src/main/java/com/rehab/exception/ApplicationException.java
+++ src/main/java/com/rehab/exception/ApplicationException.java
@@ -1,7 +1,15 @@
 package com.rehab.exception;
 
+/**
+ * Custom exception class for those exceptions that will be thrown to indicate that something went wrong.
+ */
 public class ApplicationException extends RuntimeException {
 
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message detail message.
+     */
     public ApplicationException(String message) {
         super(message);
     }
