9db491b72048adfc889c6d7c71f47e7f636d4114
toxa055 <toxa055@gmail.com>
2021-11-26 14:26:54 +0300

    Javadoc for Security.

--- src/main/java/com/rehab/config/SecurityConfig.java
+++ src/main/java/com/rehab/config/SecurityConfig.java
@@ -12,23 +12,46 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * Class that configures Spring WebSecurity.
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * Interface that retrieves user-related data.
+     */
     private final UserDetailsService userDetailsService;
 
+    /**
+     * Constructs current instance and initializes following fields.
+     *
+     * @param userDetailsService description of userDetailsService is in field declaration.
+     */
     @Autowired
     public SecurityConfig(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
+    /**
+     * Set authentication based on UserDetailsService.
+     *
+     * @param auth implementation of SecurityBuilder.
+     * @throws Exception when error occurs.
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 
+    /**
+     * Configures web based security for specific http requests.
+     *
+     * @param http parameter to configure http requests.
+     * @throws Exception when error occurs.
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -41,6 +64,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().permitAll().logoutSuccessUrl("/login");
     }
 
+    /**
+     * Method that initializes a new instance of PasswordEncoder's implementation.
+     * It's used for encoding users passwords not to keep raw passwords in database.
+     *
+     * @return new instance of BCryptPasswordEncoder with strength 12;
+     */
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
--- src/main/java/com/rehab/util/SecurityUtil.java
+++ src/main/java/com/rehab/util/SecurityUtil.java
@@ -5,13 +5,30 @@ import com.rehab.model.Employee;
 import com.rehab.security.EmployeeDetails;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+/**
+ * This class contains static utility method for getting current authenticated employee.
+ * Instantiation of this class is prohibited.
+ */
 public class SecurityUtil {
 
+    /**
+     * Private empty constructor that indicates prohibition for creating instance of current class.
+     */
     private SecurityUtil() {
     }
 
+    /**
+     * Returns current authenticated employee.
+     *
+     * @return current authenticated employee.
+     * @throws ApplicationException if error occurs.
+     */
     public static Employee getAuthEmployee() {
-        var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new ApplicationException("Cannot get auth employee.");
+        }
+        var principal = authentication.getPrincipal();
         if (principal instanceof EmployeeDetails) {
             return ((EmployeeDetails) principal).getEmployee();
         } else {
