c255eace19b6349aa49d5220168bb47ea91f7fea
toxa055 <toxa055@gmail.com>
2021-11-26 10:30:10 +0300

    Javadoc for UserDto, EventMessage.

--- src/main/java/com/rehab/dto/EventMessage.java
+++ src/main/java/com/rehab/dto/EventMessage.java
@@ -1,15 +1,52 @@
 package com.rehab.dto;
 
+import com.rehab.controller.RestEventController;
+
 import java.time.LocalTime;
 import java.util.Objects;
 
+/**
+ * Data Transfer Object for {@link com.rehab.model.Event}, i.e. it's a simplified view representation of Event model.
+ * It's used for transferring data between services and controllers.
+ * It's used for getting information about today planned events from another application through REST.
+ *
+ * @see RestEventController#getTodayPlannedEvents()
+ */
 public class EventMessage {
+
+    /**
+     * Patient insurance number.
+     */
     private Integer patientInsuranceNumber;
+
+    /**
+     * Patient name, second name.
+     */
     private String patientName;
+
+    /**
+     * Nurse name, second name.
+     */
     private String nurseName;
+
+    /**
+     * Event planned date.
+     */
     private LocalTime plannedTime;
+
+    /**
+     * Cure name.
+     */
     private String cureName;
+
+    /**
+     * Cure type.
+     */
     private String cureType;
+
+    /**
+     * Cure dose.
+     */
     private String dose;
 
     public Integer getPatientInsuranceNumber() {
@@ -68,6 +105,12 @@ public class EventMessage {
         this.dose = dose;
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param o the reference object with which to compare.
+     * @return true if this object is the same as the o argument; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,6 +125,11 @@ public class EventMessage {
                 && dose.equals(that.dose);
     }
 
+    /**
+     * Returns a hash code value for current object.
+     *
+     * @return a hash code value for the object.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(patientInsuranceNumber, patientName, nurseName, plannedTime, cureName, cureType, dose);
--- src/main/java/com/rehab/dto/UserDto.java
+++ src/main/java/com/rehab/dto/UserDto.java
@@ -5,23 +5,56 @@ import com.rehab.model.type.Role;
 import javax.validation.constraints.*;
 import java.util.Set;
 
+/**
+ * Data Transfer Object for {@link com.rehab.model.Employee}, i.e. it's a view representation of Employee model as user.
+ * It's used for transferring data between services and controllers.
+ * It's used for creating and editing employees.
+ */
 public class UserDto {
+
+    /**
+     * Employee id.
+     */
     private Integer id;
+
+    /**
+     * Employee name, second name.
+     */
     @NotBlank(message = "Name cannot be empty")
     @Size(min = 8, max = 50, message = "Length must be from 8 to 50 symbols")
     private String name;
+
+    /**
+     * Employee job position.
+     */
     @NotBlank(message = "Position cannot be empty")
     @Size(min = 3, max = 30, message = "Length must be from 3 to 30 symbols")
     private String position;
+
+    /**
+     * Employee email address.
+     */
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Incorrect email")
     private String email;
+
+    /**
+     * Employee password.
+     */
     @NotBlank(message = "Password cannot be empty")
     @Size(min = 6, max = 12, message = "Length must be from 6 to 12 symbols")
     private String password;
+
+    /**
+     * Employee password confirmation.
+     */
     @NotBlank(message = "Password confirmation cannot be empty")
     @Size(min = 6, max = 12, message = "Length must be from 6 to 12 symbols")
     private String confirmPassword;
+
+    /**
+     * Employee roles.
+     */
     @NotEmpty(message = "No one role has been selected")
     private Set<Role> roles;
 
