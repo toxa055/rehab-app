cdfe8d77419f45292c840eb681fdd4f1a3b8c6c4
toxa055 <toxa055@gmail.com>
2021-11-27 16:57:00 +0300

    Javadoc for Rest Controllers.

--- src/main/java/com/rehab/controller/RestPatientController.java
+++ src/main/java/com/rehab/controller/RestPatientController.java
@@ -4,20 +4,44 @@ import com.rehab.dto.PatientDto;
 import com.rehab.service.PatientService;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * REST controller for Patient.
+ * It's used in some jsp pages, when we need to get or send data without page reload.
+ * Any method returns execution result in JSON format in response body.
+ */
+@Secured("ROLE_DOCTOR")
 @RestController
 public class RestPatientController {
 
+    /**
+     * PatientService bean.
+     */
     private final PatientService patientService;
+
+    /**
+     * Interface that logs specific messages.
+     */
     private final Logger logger;
 
+    /**
+     * @param patientService description of patientService is in field declaration.
+     * @param logger         description of logger is in field declaration.
+     */
     @Autowired
     public RestPatientController(PatientService patientService, Logger logger) {
         this.patientService = patientService;
         this.logger = logger;
     }
 
+    /**
+     * Method executes GET request to receive patientDto by given insurance number.
+     *
+     * @param insNum patient insurance number.
+     * @return found patientDto.
+     */
     @GetMapping("/rest/patients/{insNum}")
     public PatientDto getByInsNum(@PathVariable int insNum) {
         logger.info("Get patient by insurance number {} through rest.", insNum);
